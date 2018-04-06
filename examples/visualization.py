437528642f68ce4a7ab19b79a9f4c8cbfb1af0c5
Niranjan <33854825+niranjanreddy891@users.noreply.github.com>
2018-04-06 11:56:57 +0530

    Update visualization.py

--- examples/visualization.py
+++ examples/visualization.py
@@ -1,29 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-
 import numpy as np
 import pcl
-
 import pcl.pcl_visualization
-# from pcl.pcl_registration import icp, gicp, icp_nl
+
+## @ author: niranjanreddy891
 
 cloud = pcl.load_XYZRGB('./examples/pcldata/tutorials/table_scene_mug_stereo_textured.pcd')
 visual = pcl.pcl_visualization.CloudViewing()
 
-# PointXYZ
-# visual.ShowMonochromeCloud(cloud)
 
-# visual.ShowGrayCloud(cloud, b'cloud')
 visual.ShowColorCloud(cloud, b'cloud')
 # visual.ShowColorACloud(cloud, b'cloud')
 
-# while True:
-#     visual.WasStopped()
-# end
+def check_was_stopped():
+     visual.WasStopped()
 
-flag = True
-while flag:
-    flag != visual.WasStopped()
-end
-
-
+     root.after(100, check_was_stopped)
+check_was_stopped()
